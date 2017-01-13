967e1386e7d603c0dac37ff6e974cc6ad8686487
Bruno Veiga <bspveiga@gmail.com>
2017-01-13 02:40:56 -0600

    App Strucuture -- updating package.json scripts and adding sql sample

---
+++ sample.sql
@@ -0,0 +1,143 @@
+-- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (armv7l)
+--
+-- Host: localhost    Database: devdb
+-- ------------------------------------------------------
+-- Server version	5.5.53-0+deb8u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Answers`
+--
+
+DROP TABLE IF EXISTS `Answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `option` int(11) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `QuestionId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `QuestionId` (`QuestionId`),
+  CONSTRAINT `Answers_ibfk_1` FOREIGN KEY (`QuestionId`) REFERENCES `Questions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Answers`
+--
+
+LOCK TABLES `Answers` WRITE;
+/*!40000 ALTER TABLE `Answers` DISABLE KEYS */;
+INSERT INTO `Answers` VALUES (2,1,'Pizza','2017-01-10 08:22:12','2017-01-10 08:22:12',1),(3,2,'Pasta','2017-01-10 08:22:38','2017-01-10 08:22:38',1),(4,3,'Fried Chicken','2017-01-10 08:22:55','2017-01-10 08:22:55',1),(5,1,'Chicago Bulls','2017-01-11 08:21:31','2017-01-11 08:21:31',4),(6,2,'Dallas Mavericks','2017-01-11 08:22:48','2017-01-11 08:22:48',4),(7,3,'L.A. Lakers','2017-01-11 08:26:00','2017-01-11 08:26:00',4),(8,4,'Cleveland Cavaliers','2017-01-11 08:28:49','2017-01-11 08:28:49',4);
+/*!40000 ALTER TABLE `Answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Questions`
+--
+
+DROP TABLE IF EXISTS `Questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Questions`
+--
+
+LOCK TABLES `Questions` WRITE;
+/*!40000 ALTER TABLE `Questions` DISABLE KEYS */;
+INSERT INTO `Questions` VALUES (1,'Which of these do you like the most?','2017-01-10 08:21:55','2017-01-10 08:21:55'),(2,'Which car do you prefer?','2017-01-11 04:22:45','2017-01-11 04:22:45'),(3,'What do think about JavaScript?','2017-01-11 04:23:47','2017-01-11 04:23:47'),(4,'Which of these basketball teams do you like best?','2017-01-11 08:20:26','2017-01-11 08:20:26');
+/*!40000 ALTER TABLE `Questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Responses`
+--
+
+DROP TABLE IF EXISTS `Responses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Responses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `option` int(11) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `QuestionId` int(11) DEFAULT NULL,
+  `UserId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `QuestionId` (`QuestionId`),
+  KEY `UserId` (`UserId`),
+  CONSTRAINT `Responses_ibfk_1` FOREIGN KEY (`QuestionId`) REFERENCES `Questions` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `Responses_ibfk_2` FOREIGN KEY (`UserId`) REFERENCES `Users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Responses`
+--
+
+LOCK TABLES `Responses` WRITE;
+/*!40000 ALTER TABLE `Responses` DISABLE KEYS */;
+INSERT INTO `Responses` VALUES (1,1,'Pizza','2017-01-11 05:05:08','2017-01-11 05:05:08',1,1),(2,2,'Mustang','2017-01-11 05:05:08','2017-01-11 05:05:08',2,1),(4,1,'Dodge Challenger','2017-01-11 07:33:01','2017-01-11 07:33:01',2,2);
+/*!40000 ALTER TABLE `Responses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `isAdmin` tinyint(1) DEFAULT '0',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'bspveiga@gmail.com',0,'2017-01-10 08:24:46','2017-01-10 08:24:46'),(2,'bruno@gowlikarveiga.com',0,'2017-01-11 04:20:57','2017-01-11 04:20:57'),(3,'bruno@veigastudios.com',0,'2017-01-11 04:21:20','2017-01-11 04:21:20');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-01-13  2:25:46

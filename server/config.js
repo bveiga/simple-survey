8469bb7410de8c635806caebd7263a9fc86368df
Bruno Veiga <bspveiga@gmail.com>
2017-01-13 01:53:43 -0600

    App Structure -- using js config instead, and allowing use of environment variables

---
+++ server/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	development: {
+		url: process.env.DATABASE_URL,
+		dialect: 'mysql'
+	},
+	production: {
+		url: process.env.DATABASE_URL,
+		dialect: 'mysql'
+	},
+	staging: {
+		url: process.env.DATABASE_URL,
+		dialect: 'mysql'
+	}
+};

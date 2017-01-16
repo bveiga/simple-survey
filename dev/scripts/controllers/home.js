b8b37a7cd3d57e72deaa50f030a552f4f30a1fd8
Bruno Veiga <bspveiga@gmail.com>
2017-01-16 03:52:09 -0600

    User Login -- adding authentication service, updated controllers to use it, and fixed styles

--- dev/scripts/controllers/home.js
+++ dev/scripts/controllers/home.js
@@ -7,8 +7,28 @@
  * # HomeController
  * Controller of the simpleSurveyUi
  */
-angular.module('simpleSurveyUi')
-	.controller('HomeController',['$scope', 'Page', function ($scope, Page) {
-		Page.setTitle('Simple Survey | Welcome');
-		Page.setLH(false);
-	}]);
+angular
+.module('simpleSurveyUi')
+.controller('HomeController', [
+	'$scope',
+	'$location',
+	'AuthenticationService',
+	'Page',
+function ($scope, $location, AuthenticationService, Page) {
+	Page.setTitle('Simple Survey | Welcome');
+	Page.setLH(false);
+
+	$scope.login = function() {
+		$scope.loading = true;
+		AuthenticationService.Login($scope.email, $scope.password, function (result) {
+			if(result.success === true) {
+				console.log('SUCCESS');
+				$location.path('/survey');
+			} else {
+				console.log('FAIL'+result.message);
+				$scope.error = result.message;
+				$scope.loading = false;
+			}
+		});
+	};
+}]);
---
+++ dev/scripts/services/login.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simpleSurveyUi.service:AuthenticationService
+ * @description
+ * # AuthenticationService\
+ */
+angular
+.module('simpleSurveyUi')
+.factory('AuthenticationService', ['$http', '$localStorage', function ($http, $localStorage) {
+
+	function Login(email, password, callback) {
+		$http.post('/api/authenticate', {
+			email: email,
+			password: password
+		}).then(function (response) {
+			var data = response.data;
+
+			console.log('parsed response');
+			console.dir(data);
+
+			if(data.token) {
+				/* Store user info in local storage */
+				$localStorage.surveyUser = {
+					email: data.email,
+					token: data.token
+				};
+
+				/* JWT schema for content headers */
+				$http.defaults.headers.common.Authorization = 
+					'Bearer '+ data.token;
+
+				callback({ success: true });
+			} else {
+				callback(data);
+			}
+		});
+	}
+
+	function Logout() {
+		delete $localStorage.surveyUser;
+		$http.defaults.headers.common.Authorization = '';
+	}
+	
+	var service = {};
+	service.Login = Login;
+	service.Logout = Logout;
+
+	return service;
+
+}]);

63c8c7540b5814a940467973778d5391fc39cc22
Bruno Veiga <bspveiga@gmail.com>
2017-01-18 17:18:08 -0600

    Hotfix -- issue where new user doesn't see questions

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,60 +8,77 @@ var models = require('../models');
 router.get('/:user_id/questions', function (req, res) {
 	var result= {};
 
+	/* Moved method here because Sequelize 2.x cannot handle
+		'where' calls with empty arrays */
+	function getUnrespondedQuestions(questions) {
+		/* If there aren't any unresponded questions... */
+		if(questions.length === 0) {
+			result.success = false;
+			res.json(result);
+		} else {
+			/* Get random unanswered question */
+			var randomInt = Math.floor(Math.random() * questions.length),
+			randomQuestion = questions[randomInt];
+			result.question = randomQuestion;
+
+			/* Get all related answers */
+			models.Answer.findAll({
+				where: {
+					QuestionId: randomQuestion.id
+				}
+			}).then(function (answers) {
+				result.answers = [];
+				if(answers.length === 0) {
+					result.success = false;
+				} else {
+					result.success = true;
+					answers.forEach(function (answer) {
+						result.answers.push(answer);
+					});
+				}
+				
+				res.json(result);
+			}).catch(function (error) {
+				res.status(500).json(error);
+			});
+		}
+	}
+
 	models.Response.findAll({
 		where: {
 			UserId: req.params.user_id
 		}
 	}).then(function (responses) {
+
 		/* Get list of question Ids */
-		var listResponded = responses.map(function (response) {
-			return response.QuestionId;
-		});
-		
-		/* Find all questions not responded */
-		models.Question.findAll({
-			where: {
-				id:  {
-					$notIn: listResponded
+		if(responses.length > 0) {
+			var listResponded = [];
+			listResponded = responses.map(function (response) {
+				return response.QuestionId;
+			});
+			
+			/* Find all questions not responded */
+			models.Question.findAll({
+				where: {
+					id:  {
+						$notIn: listResponded
+					}
 				}
-			}
-		}).then(function (questions) {
-
-			/* If there aren't any unresponded questions... */
-			if(questions.length === 0) {
-				result.success = false;
-				res.json(result);
-			} else {
-				/* Get random unanswered question */
-				var randomInt = Math.floor(Math.random() * questions.length),
-				randomQuestion = questions[randomInt];
-				result.question = randomQuestion;
-
-				/* Get all related answers */
-				models.Answer.findAll({
-					where: {
-						QuestionId: randomQuestion.id
-					}
-				}).then(function (answers) {
-					result.answers = [];
-					if(answers.length === 0) {
-						result.success = false;
-					} else {
-						result.success = true;
-						answers.forEach(function (answer) {
-							result.answers.push(answer);
-						});
-					}
-					
-					res.json(result);
-				}).catch(function (error) {
-					res.status(500).json(error);
-				});
-			}
-
-		}).catch(function (error) {
-			res.status(500).json(error);
-		});
+			}).then(function (questions) {
+				/* Get random question from unresponded questions */
+				getUnrespondedQuestions(questions);
+			}).catch(function (error) {
+				res.status(500).json(error);
+			});
+		} else {
+			models.Question.findAll({}).then(function (questions) {
+				/* Get random question from list of all questions */
+				getUnrespondedQuestions(questions);
+			}).catch(function (error) {
+				res.status(500).json(error);
+			});
+		}
+		
 	}).catch(function (error) {
 		res.status(500).json(error);
 	});
